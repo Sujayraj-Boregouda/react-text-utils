072fb64acf4421e33633a9e3397eab052717be38
sujayraj-supercode <sujayraj.g@supercode.in>
2023-09-19 14:17:45 +0530

    Updating logics

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,15 +33,21 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to Clipboard", "Success");     
+    }
+
   return (
     <div className='container' style={{ color: props.mode === 'dark'? 'white': 'black'}}>
         <div className="mb-3 my-3">
             <h1>{props.heading}</h1>            
-            <textarea name="" id="" cols="30" rows="10" className='form-control' value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark'? '#042743': 'white', color: props.mode === 'dark'? 'white': 'black',}}></textarea>
-            <button className="btn btn-primary my-3 mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={handleLowercase}>Convert to Lowercase</button>
-            <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={clearText}>Clear Text</button>
+            <textarea name="" id="myBox" cols="30" rows="10" className='form-control' value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark'? '#042743': 'white', color: props.mode === 'dark'? 'white': 'black',}}></textarea>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleUppercase}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleLowercase}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+            <button disabled={text.length===0} className="btn btn-primary my-3 mx-2" onClick={clearText}>Clear Text</button>
         </div>
         <div className="mb-3 my-3" style={{ color: props.mode === 'dark'? 'white': 'black'}}>
             <h1>Your Text Summary</h1>            
